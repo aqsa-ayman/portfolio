b48d0a6edbd0ce2ac032b52e69faecb04d620bb8
Aqsa <aqsa_ayman@hotmail.co.uk>
2018-02-20 20:19:22 +0000

    Add navbar and basic landing page
    Used ngx-bootstrap package for collapse functionality when on mobile devices.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { LandingPageComponent } from './core/landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
